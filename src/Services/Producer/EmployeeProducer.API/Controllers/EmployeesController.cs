a68a45a0c6bb6bc00df439ab892765acb98d71e7
Rikiricks <riky92.ce@gmail.com>
2024-10-25 10:42:37 +0530

    Add Employee Producer Service

---
+++ src/Services/Producer/EmployeeProducer.API/Controllers/EmployeesController.cs
@@ -0,0 +1,49 @@
+using Confluent.Kafka;
+using EmployeeProducer.API.Data;
+using EmployeeProducer.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+
+namespace EmployeeProducer.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeesController(EmployeeDbContext dbContext, ILogger<EmployeesController> logger) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IEnumerable<Employee>> GetEmployees()
+        {
+            logger.LogInformation("Requesting all employees");
+            return await dbContext.Employees.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Employee>> CreateEmployee(Employee employee)
+        {
+            
+            dbContext.Employees.Add(employee);
+            await dbContext.SaveChangesAsync();
+
+            var message = new Message<string, string>()
+            {
+                Key = employee.Id.ToString(),
+                Value = JsonSerializer.Serialize(employee)
+            };
+
+            var producerConfig = new ProducerConfig()
+            {
+                BootstrapServers = "kafka1:9092",
+                Acks = Acks.All,
+            };
+
+            using (var producer = new ProducerBuilder<string, string>(producerConfig).Build())
+            {
+                await producer.ProduceAsync("employeeTopic", message);
+            }
+
+            return CreatedAtAction(nameof(CreateEmployee), new { id = employee.Id }, employee);
+        }
+
+    }
+}
---
+++ src/Services/Producer/EmployeeProducer.API/Data/EmployeeDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeProducer.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeProducer.API.Data
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> dbContextOptions)
+            : base(dbContextOptions)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
